810335b8348b71ee68d68aea36ecb3cf46d10a62
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-02 18:03:13 +0200

    aggiunta funzione con IF per l'accensione e lo spegnimento della lampadina

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 const lamp = document.querySelector("#id_img");
 //Variabile bottone
 const button = document.querySelector("button");
+
+//Funzione per il cambio di immagine 
+//SE la variabile lamp include img/white_lamp.png allora si può accendere 
+//ALTRIMENTI si può spegnere 
+function lampadina() {
+    if (lamp.src.includes("img/white_lamp.png")) {
+        lamp.src = "img/yellow_lamp.png";
+        button.textContent = "spegni";
+    } else {
+        lamp.src = "img/white_lamp.png";
+        button.textContent = "accendi";
+    }
+}
+
+//Evento che richiama la funzione per accendere e spegnere la lampadina
+button.addEventListener("click", lampadina);
